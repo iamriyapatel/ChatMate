2f251faea648c1f3a5fc4e893b596b7bfb8aeba7
Riya Patel <118886756+iamriyapatel@users.noreply.github.com>
2025-02-26 19:05:44 +0530

    lets work

--- gemini-chatbot-frontend/src/App.jsx
+++ gemini-chatbot-frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Chatbot from "./components/chatbot";
 import Sidebar from "./components/sidebar";
 
 const App = () => {
-  return (
-    <div className="flex h-screen bg-[#1c1b1b] text-white">
-      {/* Sidebar */}
-      <Sidebar />
+  const [darkMode, setDarkMode] = useState(false);
 
-      {/* Main Chat Area */}
+  return (
+    <div className={`flex h-screen transition-all duration-300 ${darkMode ? "bg-black text-white" : "bg-white text-black"}`}>
+      
+      <Sidebar darkMode={darkMode} setDarkMode={setDarkMode} />
+
+      
       <div className="flex-1 h-screen">
         <Chatbot />
       </div>
