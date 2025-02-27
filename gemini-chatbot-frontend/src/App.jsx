a1e03857e2ed9aba13c2b4969a9babd0e7b62587
Riya Patel <118886756+iamriyapatel@users.noreply.github.com>
2025-02-27 14:57:15 +0530

    updated

--- gemini-chatbot-frontend/src/App.jsx
+++ gemini-chatbot-frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
-import MainSection from "./components/MainSection";
 import Sidebar from "./components/sidebar";
 import Sections from "./components/Sections";
 import Footer from "./components/Footer";
+import Chatbot from "./components/chatbot";
 
 const App = () => {
   return (
     <div className="font-sans">
       <Header />
-      <MainSection />
+      <Chatbot />
       <Sidebar />
       <Sections />
       <Footer />
